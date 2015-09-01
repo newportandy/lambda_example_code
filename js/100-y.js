62262e5fb1d58a64bd316cb61486b6dcc6501a26
Andy Newport <andy.newport@powershop.co.nz>
2015-09-01 14:13:23 +1200

    Cleanup accidental addition

--- js/100-y.js
+++ js/100-y.js
@@ -1,11 +1,11 @@
 // Simple JS factorial
-const fact = (f, x) => {
+const fact = (x) => {
   if (x === 0) {
     return 1
   } else {
-    return x * f(f, x-1)
+    return x * fact(x-1)
   }
 }
 
-console.log( fact(fact, 8) )
+console.log( fact(8) )
 //Refactor -> pass factorial function as argument to factorial.....
