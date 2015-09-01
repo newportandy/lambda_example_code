a0981ccfe4dc070ff0ca4eb7eaa0edf53c884655
Andy Newport <andy.newport@powershop.co.nz>
2015-09-01 13:21:01 +1200

    fix some spacing

--- js/100-lambda.js
+++ js/100-lambda.js
@@ -1,9 +1,9 @@
 // This is what we want to implement........
 
 const fact = (x) => {
-  if (x == 1) {      // if, true, false
+  if (x == 1) {           // if, true, false
     return 1              // numbers
-  } else {           // else
+  } else {                // else
     return x * fact(x-1)  // decrement, multiply, recursion
   }
 }
